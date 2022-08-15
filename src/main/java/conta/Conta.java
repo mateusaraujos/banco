fa1afb3ba2da20d9b47801be198884c06b829c04
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-15 19:01:05 -0300

    trabalhando com herança

---
+++ src/main/java/conta/Conta.java
@@ -0,0 +1,25 @@
+package conta;
+
+public class Conta {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar() {}
+
+    public void depositar() {}
+
+    public void transferir() {}
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
--- src/main/java/conta/ContaCorrente.java
+++ src/main/java/conta/ContaCorrente.java
@@ -1,25 +1,5 @@
 package conta;
 
-public class ContaCorrente {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaCorrente extends Conta {
 
-    public void sacar() {}
-
-    public void depositar() {}
-
-    public void transferir() {}
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
